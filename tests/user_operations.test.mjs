3532fc235be02eec1f0d1d11673a0d25d21f8e2f
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-16 17:16:04 +0300

    Install jest and create 3 test scenarios

---
+++ tests/user_operations.test.mjs
@@ -0,0 +1,111 @@
+import http, { get, request } from 'http';
+import { validate } from 'uuid';
+
+const options = {
+    host: 'host.docker.internal',
+    path: '/api/users',
+    port: 80
+}
+
+describe('User operations', () => {
+    test('Get all users', () => {
+        get(options, (request) => {
+            const data = []
+            request.on('data', (chunk) => {
+                data.push(chunk)
+            })
+
+            request.on('end', () => {
+                expect(JSON.parse(data.join())).toEqual([]);
+            })
+        });
+    })
+
+    test('Create user', () => {
+        const user = {
+            username: "Test",
+            age: 18,
+            hobbies: ["age", "hobbies"]
+        }
+
+        const requestPayload = JSON.stringify(user);
+
+        const additionalOptions = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(requestPayload),
+            }
+        }
+
+        const req = request({...options, ...additionalOptions}, (request) => {
+            const data = []
+            request.on('data', (chunk) => {
+                data.push(chunk)
+            })
+
+            request.on('end', () => {
+                const newUser = JSON.parse(data.join());
+                const {_username, _age, _hobbies, _uuid} = newUser;
+
+                expect(_username).toBeDefined();
+                expect(_username).toBe(user.username);
+
+                expect(_age).toBeDefined();
+                expect(_age).toBe(user.age);
+
+                expect(_hobbies).toBeDefined();
+                expect(_hobbies).toEqual(user.hobbies);
+
+                expect(_uuid).toBeDefined();
+                expect(validate(_uuid)).toBe(true);
+            })
+        });
+
+        req.write(requestPayload);
+        req.end()
+    });
+
+    test('Create user and get it', () => {
+        const user = {
+            username: "Test2",
+            age: 20,
+            hobbies: ["age", "hobbies"]
+        }
+
+        const requestPayload = JSON.stringify(user);
+
+        const additionalOptions = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(requestPayload),
+            }
+        }
+
+        const req = request({...options, ...additionalOptions}, (request) => {
+            const data = []
+            request.on('data', (chunk) => {
+                data.push(chunk)
+            })
+
+            request.on('end', () => {
+                const newUser = JSON.parse(data.join());
+                get({...options, path: options.path + "/" + newUser._uuid}, (req) => {
+                    const getData = [];
+                    req.on('data', (chunk) => {
+                        getData.push(chunk)
+                    });
+
+                    req.on('end', () => {
+                        const gotUser = JSON.parse(getData.join());
+                        expect(gotUser).toEqual(newUser);
+                    });
+                });
+            });
+        });
+
+        req.write(requestPayload);
+        req.end()
+    });
+});
